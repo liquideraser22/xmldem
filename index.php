68d3b1491d7464d7ca00971be0e0ab53d6b265a4
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-01-28 23:23:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,54 @@
-<!DOCTYPE html>
-<html>
-<body onload="loadXMLDoc()">
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://xmlexercise1.herokuapp.com/rss.php");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("movie");
+?>
+ <div class="title">
+ <h1>Movies</h1>
+ </div>
+ <div class="title">
 
-<h2>Using the XMLHttpRequest Object</h2>
-
-<div id="demo">
+ <div class="wrapper">
+ <?php
+ foreach( $content as $data )
+ {?>
+     <div class="border">
+     <?php
+     $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+     $actor = $data->getElementsByTagName("actor")->item(0)->nodeValue;
+     $genre = $data->getElementsByTagName("genre")->item(0)->nodeValue;
+     $director = $data->getElementsByTagName("director")->item(0)->nodeValue;
+     echo "<ul><h2>$title - $actor</h2>
+              <ul>
+                  <li>genre: $genre </li>
+                  <li>director: $director </li>
+              </ul>
+          </ul>";
+    ?>
+     </div>
+  <?php
+ }
+?>
 </div>
+</div>
+<style>
 
-<script>
-function loadXMLDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
+    .title{
+        display: flex;
+        justify-content: center;
     }
-  };
-  xhttp.open("GET", "emails.xml", true);
-  xhttp.send();
-}
-</script>
-</body>
-</html>
+    .wrapper{
+        display: flex;
+        flex-direction: column;
+        border: 2px solid blue;
+        width: 30rem;
+        border-radius: 10px;
+    }
+    .border{
+        border: 1px solid blue;
+        max-width: 50rem;
+        border-radius: 10px;
+        background-color: grey;
+    }
+</style>
